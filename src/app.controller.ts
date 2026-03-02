4a7b5bcc1e728d66c4bc042b617a0f1b9bb73c24
John-David <juandavid123456153123@gmail.com>
2026-03-02 18:36:27 -0400

    subido

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,69 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { PersonModel } from './person.models';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('personController')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
+
+  @Get('getAll')
+  getAll() {
+    return this.appService.getAll();
+  }
+
+  @Get()
+  miMetodo() {
+    return { id: 1, name: 'David', age: 30 };
+  }
+
+  @Post('saveBody')
+  savePerson(@Body() data1: any) {
+    console.log('************');
+    console.log(data1);
+    console.log('************');
+    return {
+      message: 'Se guardó correctamente',
+    };
+  }
+
+  @Get('age')
+  getAge() {
+    return { age: 30 };
+  }
+
+  @Get('getJuan')
+  getAgeJuan() {
+    const data = new PersonModel();
+    data.id = 1;
+    data.name = 'Juan';
+    data.age = 25;
+    return data;
+  }
+
+  @Get('findOne/:id')
+  findOne(@Param('id') id: number) {
+    return this.appService.findOne(id);
+  }
+
+  @Post('create')
+  create(@Body() data: PersonModel) {
+    return this.appService.create(data);
+  }
+
+  @Put('update/:id')
+  update(@Param('id') id: number, @Body() data: PersonModel) {
+    return this.appService.update(id, data);
+  }
+
+  @Delete('remove/:id')
+  remove(@Param('id') id: number) {
+    return this.appService.remove(id, {} as PersonModel);
+  }
 }
+
+//crear un crud  crear, ver eliminar, actualizar
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { PersonModel } from './person.models';
 
 @Injectable()
 export class AppService {
+  database: PersonModel[] = [
+    { id: 1, name: 'David', age: 30 },
+    { id: 2, name: 'Juan', age: 25 },
+    { id: 3, name: 'Maria', age: 28 },
+  ];
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World desde Service!';
+  }
+
+  private persons: PersonModel[] = [];
+  private idCounter = 4;
+
+  create(person: PersonModel) {
+    person.id = this.idCounter++;
+    this.database.push(person);
+    return person;
+  }
+
+  getAll() {
+    return this.database;
+  }
+
+  findOne(id: number) {
+    return this.database.find(p => p.id == id);
+  }
+
+  update(id: number, data: PersonModel) {
+    const person = this.findOne(id);
+    if (person) {
+      person.name = data.name;
+      person.age = data.age;
+      return person;
+    }
+    return null;
+  }
+
+  remove(id: number) {
+    const index = this.database.findIndex(p => p.id === Number(id));
+    if (index !== -1) {
+      this.database.splice(index, 1);
+      return { message: 'Eliminado correctamente' };
+    }
+    return { message: 'No encontrado' };
   }
 }
